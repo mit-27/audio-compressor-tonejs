876735966d6766638309a3fe2c32eab4477b5912
mit-27 <meetsuthar64@gmail.com>
2024-09-20 10:37:39 -0600

    💚 Fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import CompressionPlayer from "@/components/CompressionPlayer";
-import GithubButton from "@/components/github-button";
 import HeroSection from "@/components/hero-section";
 import DotPattern from "@/components/ui/dot-pattern";
-import { cn } from "@/lib/utils";
 
 const Home = () => {
   return (
