15bd6bb47240c416388eb326143f5c02fc41ffa7
mit-27 <meetsuthar64@gmail.com>
2024-09-20 09:54:21 -0600

    🎨 Update Github button

--- src/components/github-button.tsx
+++ src/components/github-button.tsx
@@ -1,11 +1,21 @@
 "use client"
 
 import PulsatingButton from "./magicui/pulsating-button"
+import { GitHubLogoIcon } from "@radix-ui/react-icons"
 
 const GithubButton = () => {
   return (
-    <PulsatingButton onClick={() => window.open('https://github.com/mit-27/audio-compressor-tonejs', '_blank')} pulseColor="gray" color="black" className="text-white bg-black m-5">Visit Github</PulsatingButton>
-
+    <PulsatingButton 
+      onClick={() => window.open('https://github.com/mit-27/audio-compressor-tonejs', '_blank')} 
+      pulseColor="gray" 
+      color="black" 
+      className="text-white bg-black m-5"
+    >
+      <div className="flex items-center justify-center gap-2">
+        <GitHubLogoIcon className="w-5 h-5" />
+        <span>Star on Github</span>
+      </div>
+    </PulsatingButton>
   )
 }
 
