8a792f17b4ebb15e4bd159cee6938cc4ef829818
mit-27 <meetsuthar64@gmail.com>
2024-09-19 18:33:24 -0600

    ✨ Add mp3 player tracker using Transport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import TonePlayer from "@/components/TonePlayer";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Click Me</Button>
+      <TonePlayer/>
     </div>
   );
 }
